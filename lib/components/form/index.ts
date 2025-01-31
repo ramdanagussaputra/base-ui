b54691e0592830df98ed69f21737a2792a6bd66a
Ramdan Agus Saputra <ramdan@massivemusic.co.id>
2025-01-31 14:41:27 +0700

    fix: unexported CheckboxFormField

--- lib/components/form/index.ts
+++ lib/components/form/index.ts
@@ -1,3 +1,4 @@
 export { Fieldset } from "#/components/form/components/fieldset/Fieldset";
 export { TextFormField } from "#/components/form/components/form-field/TextFormField";
 export { PasswordFormField } from "#/components/form/components/form-field/PasswordFormField";
+export { CheckboxFormField } from "#/components/form/components/form-field/CheckboxFormField";
