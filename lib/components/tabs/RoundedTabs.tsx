466a936b8c1178a0275335ac01a28a1d327278f4
Ramdan Agus Saputra <ramdan@massivemusic.co.id>
2025-02-11 17:20:59 +0700

    fix(tabs): padding for rounded tabs

--- lib/components/tabs/RoundedTabs.tsx
+++ lib/components/tabs/RoundedTabs.tsx
@@ -20,7 +20,7 @@ export function RoundedTabs({ tabs, onTabClick }: TabsProps) {
       <TabList className="flex w-full items-end gap-(--rounded-tabs-gap) outline-hidden">
         {tabs.map((tab) => (
           <Tab
-            className="cursor-pointer rounded-full border border-(--rounded-tabs-border-color) p-(--rounded-tabs-padding) text-(length:--rounded-tabs-font-size) leading-(--rounded-tabs-line-height) font-(--rounded-tabs-font-weight) whitespace-nowrap text-(--rounded-tabs-border-color) outline-hidden duration-100 data-selected:bg-(--rounded-tabs-bg-color--active) data-selected:text-(--rounded-tabs-color--active)"
+            className="cursor-pointer rounded-full border border-(--rounded-tabs-border-color) px-(--rounded-tabs-padding-horizontal) py-(--rounded-tabs-padding-vertical) text-(length:--rounded-tabs-font-size) leading-(--rounded-tabs-line-height) font-(--rounded-tabs-font-weight) whitespace-nowrap text-(--rounded-tabs-border-color) outline-hidden duration-100 data-selected:bg-(--rounded-tabs-bg-color--active) data-selected:text-(--rounded-tabs-color--active)"
             key={tab.name}
           >
             {tab.name}
