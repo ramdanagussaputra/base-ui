4bec3da0253e0497dfa31d10b670b36298ac4cff
Ramdan Agus Saputra <ramdan@massivemusic.co.id>
2025-03-03 17:03:27 +0700

    fix: add classname props

--- lib/components/table/TableFooter.tsx
+++ lib/components/table/TableFooter.tsx
@@ -1,11 +1,21 @@
+import { cn } from "#/utils";
+
 interface TableFooterProps {
   children: React.ReactNode;
   className?: string;
 }
 
-export function TableFooter({ children }: Readonly<TableFooterProps>) {
+export function TableFooter({
+  children,
+  className,
+}: Readonly<TableFooterProps>) {
   return (
-    <footer className="flex items-center gap-5 border-t border-(--table-border-color) px-5 py-[1.0625rem]">
+    <footer
+      className={cn(
+        "flex items-center gap-5 border-t border-(--table-border-color) px-5 py-[1.0625rem]",
+        className,
+      )}
+    >
       {children}
     </footer>
   );
