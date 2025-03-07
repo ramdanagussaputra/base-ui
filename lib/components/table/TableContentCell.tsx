1d641fbf458b5d2ca5ab5612ad628f39877cbb40
Ramdan Agus Saputra <ramdan@massivemusic.co.id>
2025-03-07 16:46:58 +0700

    fix: add width className

--- lib/components/table/TableContentCell.tsx
+++ lib/components/table/TableContentCell.tsx
@@ -4,6 +4,7 @@ interface TableContentCellProps {
   children: React.ReactNode;
   className?: string;
   innerContainerClassName?: string;
+  widthClassName?: string;
   isNoWrap?: boolean;
   onClick?: () => void;
 }
@@ -14,16 +15,22 @@ export function TableContentCell({
   className,
   innerContainerClassName,
   isNoWrap,
+  widthClassName,
 }: Readonly<TableContentCellProps>) {
   return (
     <td
-      className={cn("bg-(--table-cell-bg-color) p-0", className)}
+      className={cn(
+        "bg-(--table-cell-bg-color) p-0",
+        className,
+        widthClassName,
+      )}
       onClick={onClick}
     >
       <div
         className={cn(
           "text-primary-700 h-(--table-cell-height) px-5 py-3 text-(length:--table-cell-font-size) leading-(--table-cell-line-height) font-normal text-(--table-cell-color) duration-100",
           innerContainerClassName,
+          widthClassName,
         )}
       >
         <span
