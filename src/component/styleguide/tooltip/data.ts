04cd91694df7a09911c341c3e032473dc6b678b9
Imam Faraz Aditya <imamaditya2211@gmail.com>
2025-06-12 14:18:37 +0700

    feat: add progress tooltip data and configuration

--- src/component/styleguide/tooltip/data.ts
+++ src/component/styleguide/tooltip/data.ts
@@ -1,6 +1,13 @@
+import { ProgressTooltipContent } from "#/components/tooltip/model";
+
 type TooltipConfig = {
   position: "top" | "bottom" | "left" | "right";
   message: string;
+};
+
+type ProgressTooltipConfig = {
+  position: "left" | "right";
+  content: ProgressTooltipContent[];
 };
 
 export const tooltipConfigs: TooltipConfig[] = [
@@ -19,5 +26,37 @@ export const tooltipConfigs: TooltipConfig[] = [
   {
     position: "right",
     message: "Tooltip message",
+  },
+];
+
+export const progressTooltipData: ProgressTooltipContent[] = [
+  {
+    title: "New",
+    description: "Project created",
+    status: "completed",
+    date: new Date(),
+  },
+  {
+    title: "Contracted",
+    description: "Waiting for contract creation",
+    status: "in-progress",
+    date: null,
+  },
+  {
+    title: "Invoiced",
+    description: "Waiting for invoice creation",
+    status: "pending",
+    date: null,
+  },
+];
+
+export const progressTooltipConfigs: ProgressTooltipConfig[] = [
+  {
+    position: "left",
+    content: progressTooltipData,
+  },
+  {
+    position: "right",
+    content: progressTooltipData,
   },
 ];
