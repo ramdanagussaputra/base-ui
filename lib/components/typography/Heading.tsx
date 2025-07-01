4aec3416b551fca70bc8d49c7be90197e190694c
Ramdan Agus Saputra <ramdan@massivemusic.co.id>
2025-07-01 14:48:12 +0700

    chore(dependencies): update React and related packages to version 19.1.0; upgrade devDependencies including ESLint, TypeScript, and Vite

--- lib/components/typography/Heading.tsx
+++ lib/components/typography/Heading.tsx
@@ -1,5 +1,5 @@
 import { cn } from "#/utils";
-import { ReactNode } from "react";
+import { ReactNode, JSX } from "react";
 
 interface HeadingProps {
   readonly as: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
