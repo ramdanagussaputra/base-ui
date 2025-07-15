79f93d8af46f70006a94765432bf85e0d064651c
Imam Faraz Aditya <imamaditya2211@gmail.com>
2025-07-15 16:13:13 +0700

    fix(SingleAvatarSection): adjust gap sizes for avatar alignment and update display numbers for consistency

--- src/component/styleguide/avatars/single-avatar/SingleAvatar.tsx
+++ src/component/styleguide/avatars/single-avatar/SingleAvatar.tsx
@@ -6,22 +6,22 @@ function SingleAvatarSection() {
     <div className="space-y-5">
       <StyleguideSubtitle>Single Avatars</StyleguideSubtitle>
       <div className="flex flex-col gap-5">
-        <div className="flex items-center gap-5">
+        <div className="flex items-center gap-3">
           <SingleAvatar size="small" src="https://i.pravatar.cc/300" />
           <SingleAvatar size="medium" src="https://i.pravatar.cc/301" />
           <SingleAvatar size="large" src="https://i.pravatar.cc/302" />
         </div>
 
-        <div className="flex items-center gap-5">
+        <div className="flex items-center gap-3">
           <SingleAvatar size="small" />
           <SingleAvatar size="medium" />
           <SingleAvatar size="large" />
         </div>
 
-        <div className="flex items-center gap-5">
+        <div className="flex items-center gap-3">
           <SingleAvatar size="small" displayNumber={100} />
-          <SingleAvatar size="medium" displayNumber={100} />
-          <SingleAvatar size="large" displayNumber={100} />
+          <SingleAvatar size="medium" displayNumber={110} />
+          <SingleAvatar size="large" displayNumber={120} />
         </div>
       </div>
     </div>
