e92a7019d972ca6026a00fc671edd89c6f2264d4
Imam Faraz Aditya <imamaditya2211@gmail.com>
2025-07-15 16:13:08 +0700

    refactor(MultipleAvatarSection): move exampleSrcs to a separate data file for better organization

--- src/component/styleguide/avatars/multiple-avatar/MultipleAvatar.tsx
+++ src/component/styleguide/avatars/multiple-avatar/MultipleAvatar.tsx
@@ -1,41 +1,7 @@
 import { MultipleAvatar } from "massive-base-ui";
 
+import { exampleSrcs } from "@/component/styleguide/avatars/multiple-avatar/data";
 import StyleguideSubtitle from "@/component/styleguide/StyleguideSubtitle";
-
-const exampleSrcs = [
-  {
-    src: "https://i.pravatar.cc/300",
-    alt: "Avatar 1",
-  },
-  {
-    src: "https://i.pravatar.cc/301",
-    alt: "Avatar 2",
-  },
-  {
-    src: "https://i.pravatar.cc/302",
-    alt: "Avatar 3",
-  },
-  {
-    src: "https://i.pravatar.cc/303",
-    alt: "Avatar 4",
-  },
-  {
-    src: "https://i.pravatar.cc/304",
-    alt: "Avatar 5",
-  },
-  {
-    src: "https://i.pravatar.cc/305",
-    alt: "Avatar 6",
-  },
-  {
-    src: "https://i.pravatar.cc/306",
-    alt: "Avatar 7",
-  },
-  {
-    src: "https://i.pravatar.cc/307",
-    alt: "Avatar 8",
-  },
-];
 
 function MultipleAvatarSection() {
   return (
@@ -45,7 +11,7 @@ function MultipleAvatarSection() {
       <div className="flex flex-col gap-5">
         <MultipleAvatar srcs={exampleSrcs} size="small" />
         <MultipleAvatar srcs={exampleSrcs} size="medium" />
-        <MultipleAvatar srcs={exampleSrcs} size="large" />
+        <MultipleAvatar srcs={exampleSrcs} size="large" maxDisplayCount={1} />
       </div>
     </div>
   );
