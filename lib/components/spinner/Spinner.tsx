8574bd984997ab6a58765426519c4a7fe9b235b6
Ramdan Agus Saputra <ramdan@massivemusic.co.id>
2025-07-31 14:40:56 +0700

    fix: adjust Spinner component to set dynamic width and height based on size prop

--- lib/components/spinner/Spinner.tsx
+++ lib/components/spinner/Spinner.tsx
@@ -50,7 +50,7 @@ export function Spinner({ size = 32, color = "default" }: Readonly<SpinnerProps>
   };
 
   return (
-    <div className="relative flex w-fit items-center justify-center rounded-xl">
+    <div className="relative flex items-center justify-center rounded-xl" style={{ width: size, height: size }}>
       <div
         className={cn(
           "absolute rounded-full border-[3px] opacity-20",
