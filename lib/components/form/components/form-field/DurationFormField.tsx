855115ba7b0271e5aac275d25ccfc43e09ed7956
Ramdan Agus Saputra <ramdan@massivemusic.co.id>
2025-08-11 15:06:36 +0700

    fix(DurationFormField): remove unnecessary props and utilize useFormContext for setValue and watch

--- lib/components/form/components/form-field/DurationFormField.tsx
+++ lib/components/form/components/form-field/DurationFormField.tsx
@@ -1,22 +1,15 @@
 // WARNING: This component should use within FormProvider from react-hook-form. learn how to use it in https://react-hook-form.com/docs/formprovider
 
 import * as React from "react";
-import { Controller } from "react-hook-form";
+import { Controller, useFormContext } from "react-hook-form";
 
 import { Fieldset } from "#/components/form/components/fieldset/Fieldset";
 import { FormFieldProps } from "#/components/form/model";
 
-import { UseFormSetValue, UseFormWatch } from "react-hook-form";
-
 type DurationFormFieldProp = Omit<FormFieldProps, "type" | "onChange"> & {
-  /** Called when the duration value changes ("mmm:ss") */
   onChange?: (value: string) => void;
-  /** Optional element to display at the end of each input */
   endElement?: React.ReactNode;
-  /** Function to set the value in the parent form context */
-  setValue: UseFormSetValue<any>;
-  /** Function to watch the value in the parent form context */
-  watch: UseFormWatch<any>;
+  name: string;
 };
 
 export function DurationFormField({
@@ -31,9 +24,8 @@ export function DurationFormField({
   size = "medium",
   withoutTagLabel = false,
   endElement,
-  setValue,
-  watch,
 }: Readonly<DurationFormFieldProp>) {
+  const { setValue, watch } = useFormContext();
   const formValue = watch(name) || "";
   const [minutes, setMinutes] = React.useState("");
   const [seconds, setSeconds] = React.useState("");
--- src/component/styleguide/forms/InputFieldForm.tsx
+++ src/component/styleguide/forms/InputFieldForm.tsx
@@ -10,7 +10,7 @@ import StyleguideSubtitle from "@/component/styleguide/StyleguideSubtitle";
 import { useFormContext } from "react-hook-form";
 
 function InputFieldForm() {
-  const { control, watch, setValue } = useFormContext();
+  const { control, watch } = useFormContext();
   const duration = watch("duration") || "";
   console.log(duration, "duration");
 
@@ -87,8 +87,6 @@ function InputFieldForm() {
           label="Duration input"
           name="duration"
           placeholder="Enter duration"
-          setValue={setValue}
-          watch={watch}
         />
       </div>
     </>
