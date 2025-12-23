0915d60a8bdb8b294c7b8c0fdd195636c4cd27f3
Ramdan Agus Saputra <ramdan@massivemusic.co.id>
2025-12-23 14:50:20 +0700

    refactor: Convert HeaderLanding to a forwardRef component for improved ref handling

--- lib/components/landing/header/HeaderLanding.tsx
+++ lib/components/landing/header/HeaderLanding.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { forwardRef, ReactNode } from "react";
 import { cn } from "#/utils";
 import composyncLogo from "#/components/landing/header/assets/composync.svg";
 
@@ -8,27 +8,28 @@ interface HeaderLandingProps {
   children?: ReactNode;
 }
 
-function HeaderLanding({
-  className,
-  brandLogo = composyncLogo,
-  children,
-}: HeaderLandingProps) {
-  return (
-    <header
-      className={cn(
-        `flex h-[5.5rem] w-full items-center justify-between`,
-        className,
-      )}
-    >
-      <img
-        src={brandLogo}
-        alt="Brand Logo"
-        className="h-[1.5rem] w-[13.740625rem]"
-      />
+const HeaderLanding = forwardRef<HTMLElement, HeaderLandingProps>(
+  ({ className, brandLogo = composyncLogo, children }, ref) => {
+    return (
+      <header
+        ref={ref}
+        className={cn(
+          `flex h-[5.5rem] w-full items-center justify-between`,
+          className,
+        )}
+      >
+        <img
+          src={brandLogo}
+          alt="Brand Logo"
+          className="h-[1.5rem] w-[13.740625rem]"
+        />
 
-      {children}
-    </header>
-  );
-}
+        {children}
+      </header>
+    );
+  },
+);
+
+HeaderLanding.displayName = "HeaderLanding";
 
 export default HeaderLanding;
